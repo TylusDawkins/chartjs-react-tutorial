f85b18b9f2a4d3a56d457344b8fa91168e550358
TylusDawkins <100214696+TylusDawkins@users.noreply.github.com>
2022-08-11 12:51:28 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -3,15 +3,39 @@ import "./App.css";
 import BarChart from "./components/BarChart";
 import LineChart from "./components/LineChart";
 import PieChart from "./components/PieChart";
-import { UserData } from "./Data";
+import { MonthsData } from "./Data";
+import { Sales } from "./Sales"
+
 
 function App() {
-  const [userData, setUserData] = useState({
-    labels: UserData.map((data) => data.year),
+    const now = new Date()
+    const yesterday = new Date(new Date().getTime() - (1 * 24 * 60 * 60 * 1000))
+    const last7Days = new Date(new Date().getTime() - (7 * 24 * 60 * 60 * 1000))
+    const lastMonth = new Date(new Date().getTime() - (30 * 24 * 60 * 60 * 1000))
+    const last6Months = new Date(new Date().getTime() - (182 * 24 * 60 * 60 * 1000))
+    const lastYear = new Date(new Date().getTime() - (365 * 24 * 60 * 60 * 1000))
+
+    console.log(Date.parse(Sales[0].effective_date).toString())
+
+    console.log(Date.parse(now))
+    console.log(Date.parse(yesterday))
+    console.log(Date.parse(lastMonth))
+    console.log(Date.parse(lastYear))
+
+    // take the dates, parse them all for UTC miliseconds and compare them
+
+    // console.log(last7Days)
+    // console.log(lastMonth)
+    // console.log(last6Months )
+    // console.log(lastYear)
+  
+
+  const [monthsData, setMonthsData] = useState({
+    labels: MonthsData.map((data) => data.month),
     datasets: [
       {
-        label: "Users Gained",
-        data: UserData.map((data) => data.userGain),
+        label: "Total Sales",
+        data: MonthsData.map((data) => data.totalSales),
         backgroundColor: [
           "rgba(75,192,192,1)",
           "#ecf0f1",
@@ -25,18 +49,38 @@ function App() {
     ],
   });
 
+  const [userSales, setuserSales] = useState({
+    labels: Sales.map((data) => data.result),
+    datasets: [
+      {
+        label: "Result",
+        data: Sales.map((data) => data.result),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
+
+  // console.log(Sales)
   // IF YOU SEE THIS COMMENT: I HAVE GOOD EYESIGHT
 
   return (
     <div className="App">
       <div style={{ width: 700 }}>
-        <BarChart chartData={userData} />
+        <BarChart chartData={monthsData} />
       </div>
       <div style={{ width: 700 }}>
-        <LineChart chartData={userData} />
+        <LineChart chartData={monthsData} />
       </div>
       <div style={{ width: 700 }}>
-        <PieChart chartData={userData} />
+        <PieChart chartData={monthsData} />
       </div>
     </div>
   );
---
+++ src/Sales.js
@@ -0,0 +1,40 @@
+export const Sales = [
+    {
+      policy_number: 10000,
+      policy_type: "Auto",
+      effective_date: "08/12/2022",
+      premium: 976,
+      result: "CC",
+      reason: null,
+    },
+    {
+        policy_number: 10001,
+        policy_type: "Home",
+        
+        premium: 2176,
+        result: "Sale",
+        reason: null,
+      },
+      {
+        policy_number: 10002,
+        policy_type: "Auto",
+        premium: 456,
+        result: "Decline",
+        reason: null,
+      },
+      {
+        policy_number: 10003,
+        policy_type: "Auto",
+        premium: 938,
+        result: "DCL",
+        reason: null,
+      },
+      {
+        policy_number: 10004,
+        policy_type: "Auto",
+        premium: 976,
+        result: "INC",
+        reason: null,
+      },
+  ];
+  
